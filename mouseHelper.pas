72f96dd3eb071e210f0bbde19a1712b175e84a46
vhanla <vhanla@gmail.com>
2023-08-17 16:17:29 -0500

    Add wndproc to handle messages from dll

--- mouseHelper.pas
+++ mouseHelper.pas
@@ -3,15 +3,26 @@ unit mouseHelper;
 interface
 
 uses
-  Vcl.ExtCtrls, System.Classes, Winapi.Windows, Winapi.Messages, Vcl.Forms, Winapi.ShellAPI;
+  Vcl.ExtCtrls, System.Classes, Winapi.Windows, Winapi.Messages, Vcl.Forms,
+  Winapi.ShellAPI, Generics.Collections;
 
 type
-  // Dynamically load the WinXHelper mouse WM_MOUSE global hook
+  TRunHook = procedure(Handle: HWND); cdecl;
+  TKillHook = procedure; cdecl;
 
+  // Monitor's handling
+  TMsonitor = record
+    FMonitorRect: TRect;
+  end;
+
+  // Dynamically load the WinXHelper mouse WM_MOUSE global hook
+  THotMouseEvent = (meTopLeft, meTop, meTopRigh, meLeft, meBotLeft, meBot, meBotRight);
 
   // A non visible component that has WNDProc to receive messages from MouseHook.dll
-  TMouseHelper = class(TComponent)
+  THotMouse = class(TComponent)
   private
+    FMonitors: TList<TMonitor>;
+
     FInterval: Cardinal;
     FWindowHandle: HWND;
     FOnTimer: TNotifyEvent;
@@ -22,6 +33,33 @@ type
     FParentWindow: HWND;
     FPrevWndProc: Pointer;
     FWndProcMethod: TWndMethod;
+
+    // Handling optional (but recommended) dll global mouse hook
+    FUseDllHook: Boolean; // This is better than the timer itself than of polling mousepos calls each time
+    RunHook: TRunHook;
+    KillHook: TKillHook;
+    DllHandle: HMODULE;
+    // However, it won't return values if this app runs non elevated while foreground window is elevated
+    // IDEA: it run non elevated, let's fallback to normal timer method
+    // but I guess it will require to use another hook to check window focus changes
+    // and most likely another (x64 hook too) platform, even though Win11 is 64 bit only
+    // yet there runs x86 apps.
+
+    // Hot spot detection
+    FCursorOnBlank: Boolean;
+    FCursorOnBlankTimeStamp: Cardinal; // last time seen in that position
+    FCursorOnHotSpot: Boolean;
+    FCursorOnHotSpotTimeStamp: Cardinal; // last time seen in that position
+    FCursorOnKnockSpot: Boolean;
+    FCursorOnKnockSpotTimeStamp: Cardinal; // last time seen in that location
+    FCursorOnKnockCount: Integer; // times to knock this hot spot
+    // Shift keys to monitor, only considered if they're held since a blank area, not in the hotspot
+    FShiftPressedFromBlank: Boolean;
+    FAltPressedFromBlank: Boolean;
+    FCtrlPressedFromBlank: Boolean;
+
+    procedure UpdateMonitors;
+
     procedure SubWndProc(var Msg: TMessage);
     procedure UpdateTimer;
     procedure SetEnabled(Value: Boolean);
@@ -44,9 +82,12 @@ type
 
 implementation
 
+uses
+  sharedDefs;
+
 { TMouseHelper }
 
-constructor TMouseHelper.Create(AOwner: TComponent);
+constructor THotMouse.Create(AOwner: TComponent);
 begin
   inherited Create(AOwner);
   FEnabled := True;
@@ -60,10 +101,15 @@ begin
 
   // SubClass the parent Form (class name WinXCorners in order to receive messages from WinXHelper.dll
   FPrevWndProc := Pointer(SetWindowLongPtr(FParentWindow, GWLP_WNDPROC, LONG_PTR(FHiddenWindowHandle)));
+
+  // Monitor's handling
+  FMonitors := TList<TMonitor>.Create;
 end;
 
-destructor TMouseHelper.Destroy;
+destructor THotMouse.Destroy;
 begin
+  FMonitors.Free;
+
   // Restore the original WndProc before deallocating the hidden window
   SetWindowLongPtr(FParentWindow, GWLP_WNDPROC, LONG_PTR(FPrevWndProc));
   DeallocateHWnd(FHiddenWindowHandle);
@@ -79,7 +125,7 @@ begin
   inherited Destroy;
 end;
 
-procedure TMouseHelper.SetEnabled(Value: Boolean);
+procedure THotMouse.SetEnabled(Value: Boolean);
 begin
   if Value <> FEnabled then
   begin
@@ -88,7 +134,7 @@ begin
   end;
 end;
 
-procedure TMouseHelper.SetInterval(Value: Cardinal);
+procedure THotMouse.SetInterval(Value: Cardinal);
 begin
   if Value <> FInterval then
   begin
@@ -97,19 +143,50 @@ begin
   end;
 end;
 
-procedure TMouseHelper.SetOnTimer(Value: TNotifyEvent);
+procedure THotMouse.SetOnTimer(Value: TNotifyEvent);
 begin
   FOnTimer := Value;
   UpdateTimer;
 end;
 
-procedure TMouseHelper.SubWndProc(var Msg: TMessage);
+procedure THotMouse.SubWndProc(var Msg: TMessage);
+var
+  Mods: TKeyStatus;
+  Data: PCopyDataStruct;
+  Mous: PMouseHookStruct;
+  CopyDataHandled: Boolean;
 begin
+  CopyDataHandled := False;
+
+  case Msg.Msg of
+    WM_KEYSTATUSCHANGED:
+    begin
+      Mods := PKeyStatus(Msg.LParam)^;
+    end;
+    WM_COPYDATA:
+    begin
+      Msg.Result := 0;
+      Data := PCopyDataStruct(Msg.LParam);
+      if Data <> nil then
+      begin
+        Mous := PMouseHookStruct(Data.lpData);
+        if Mous <> nil then
+        begin
+          FX := Mous^.pt.X;
+          FY := Mous^.pt.Y;
+        end;
+        CopyDataHandled := True;
+      end;
+      Msg.Result := 1;
+    end;
+  end;
+
   // Call the original WndProc to ensure normal message handling
+  if not CopyDataHandled then
   Msg.Result := CallWindowProc(FPrevWndProc, FParentWindow, Msg.Msg, Msg.WParam, Msg.LParam);
 end;
 
-procedure TMouseHelper.Timer;
+procedure THotMouse.Timer;
 var
   MPos: TPoint;
   Cur: THandle;
@@ -143,7 +220,24 @@ begin
 
 end;
 
-procedure TMouseHelper.UpdateTimer;
+procedure THotMouse.UpdateMonitors;
+var
+  I: Integer;
+  mc: Integer;
+begin
+  FMonitors.Clear;
+
+  mc := Screen.MonitorCount;
+  if mc > 0 then
+  begin
+    for I := 0 to mc - 1 do
+    begin
+      FMonitors.Add(Screen.Monitors[I]);
+    end;
+  end;
+end;
+
+procedure THotMouse.UpdateTimer;
 begin
   KillTimer(FWindowHandle, 1);
   if (FInterval <> 0) and FEnabled and Assigned(FOnTimer) then
@@ -151,7 +245,7 @@ begin
       raise EOutOfResources.Create('No timers available');
 end;
 
-procedure TMouseHelper.WndProc(var Msg: TMessage);
+procedure THotMouse.WndProc(var Msg: TMessage);
 begin
   case Msg.Msg of
     WM_TIMER:
@@ -177,7 +271,25 @@ begin
             end;
         end;
       end;
-
+    WM_DISPLAYCHANGE: // Display resolution change
+      begin
+        // Update Monitors count, resolution
+        UpdateMonitors;
+      end;
+    WM_SETTINGCHANGE: // look for WorkArea or Wallpaper event, those are related to monitor changes (add/remove)
+      begin
+        if (Msg.WParam = SPI_SETWORKAREA) or (Msg.WParam = SPI_SETDESKWALLPAPER)  then
+        begin
+          // Handle monitor change
+          UpdateMonitors;
+        end;
+      end;
+    WM_DPICHANGED:
+      begin
+        // Update ScaleFactor ??? (it might not be necessary
+        UpdateMonitors;
+      end;
+    //------------- DEFAULT ------------//
     else
     begin
 
