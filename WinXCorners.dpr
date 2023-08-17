f22fa3c8a9c124ffa2b685c90065db6772bb0970
vhanla <vhanla@gmail.com>
2023-08-17 16:19:07 -0500

    Add more files

--- WinXCorners.dpr
+++ WinXCorners.dpr
@@ -23,6 +23,8 @@ program WinXCorners;
 {$R *.dres}
 
 uses
+  FMX.Forms,
+  Skia.FMX,
   madExcept,
   madLinkDisAsm,
   madListHardware,
@@ -35,13 +37,27 @@ uses
   XCombobox in 'XCombobox.pas',
   osdgui in 'osdgui.pas' {frmOSD},
   frmAdvanced in 'frmAdvanced.pas' {frmAdvSettings},
-  functions in 'functions.pas';
+  functions in 'functions.pas',
+  frmWinTiles in 'frmWinTiles.pas' {wndListWindows},
+  actionsManager in 'actionsManager.pas',
+  Vcl.Themes,
+  Vcl.Styles,
+  InfDesktopWallpaper in 'InfDesktopWallpaper.pas',
+  frmMouseShake in 'frmMouseShake.pas' {formMouseShake},
+  mouseHelper in 'mouseHelper.pas',
+  fmxSettings in 'fmxSettings.pas' {fmxSettingsForm},
+  fmxLaunchpad in 'fmxLaunchpad.pas' {Form1},
+  taskbarHelper in 'taskbarHelper.pas',
+  settingsHelper in 'settingsHelper.pas';
+
+//  fmxStageManager in 'fmxStageManager.pas' {Form1};
 
 {$R *.res}
 
 var
   AppHandle: HWND;
 begin
+  GlobalUseSkia := True;
   AppHandle := FindWindow('WinXCorners', nil);
   if AppHandle > 0 then
   begin
@@ -52,10 +68,17 @@ begin
   Application.Initialize;
   Application.ShowMainForm := False;
   Application.MainFormOnTaskbar := False;
+  TStyleManager.TrySetStyle('Windows11 Polar Dark');
   Application.Title := 'WinXCorners';
   Application.CreateForm(TfrmMain, frmMain);
   Application.CreateForm(TfrmTrayPopup, frmTrayPopup);
   Application.CreateForm(TfrmOSD, frmOSD);
   Application.CreateForm(TfrmAdvSettings, frmAdvSettings);
+  Application.CreateForm(TwndListWindows, wndListWindows);
+  Application.CreateForm(TformMouseShake, formMouseShake);
+  Application.CreateForm(TfmxSettingsForm, fmxSettingsForm);
+  Application.CreateForm(TForm1, Form1);
+  //  formMouseShake.Show;
+  //  Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
