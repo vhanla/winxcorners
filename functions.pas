82426aee917234779e35e0cc4b9a32c2a14e82ce
vhanla <vhanla@gmail.com>
2024-12-02 00:36:18 -0500

    💡 Add Raw Input functions and constants.

--- functions.pas
+++ functions.pas
@@ -2,6 +2,8 @@
 Functions that we will be using
 
 Changelog:
+24-12-02
+- Add Raw Input constants and functions
 24-07-27
 - Add undocumented DwmpGetColorizationParameters DWMapi.dll to get
   the RGB values directly instead of precalculated with other params
@@ -68,6 +70,78 @@ type
     wReserved: BYTE;
   end;
   RTL_OSVERSIONINFOEXW = _OSVERSIONINFOEXW;
+
+// RAW INPUT
+const
+  RIM_TYPEHID      = 2;
+  RIM_TYPEKEYBOARD = 1;
+  RIM_TYPEMOUSE    = 0;
+
+  RID_INPUT  = $10000003;
+  HID_USAGE_PAGE_GENERIC       = $01;
+  HID_USAGE_GENERIC_MOUSE      = $02;
+type
+  HRAWINPUT = THandle;
+
+  tagRAWINPUTDEVICE = record
+    usUsagePage: Word;
+    usUsage: Word;
+    dwFlags: DWORD;
+    hwndTarget: HWND;
+  end;
+  RAWINPUTDEVICE = tagRAWINPUTDEVICE;
+
+  PRAWINPUTDEVICE = ^RAWINPUTDEVICE;
+
+  tagRAWINPUTHEADER = record
+    dwType: DWORD;
+    dwSize: DWORD;
+    hDevice: THandle;
+    wParam: WPARAM;
+  end;
+  RAWINPUTHEADER = tagRAWINPUTHEADER;
+
+  tagRAWKEYBOARD = record
+    MakeCode: Word;
+    Flags: Word;
+    Reserved: Word;
+    VKey: Word;
+    Message: UINT;
+    ExtraInformation: ULONG;
+  end;
+  RAWKEYBOARD = tagRAWKEYBOARD;
+
+  tagRAWMOUSE = record
+    usFlags:  Word;
+    case Integer of
+      0:  (ulButtons: ULONG);
+      1:  (usButtonFlags: Word;
+           usButtonsData: Word;
+    ulRawButtons: ULONG;
+    lLastX: Longint;
+    lLastY: Longint;
+    ulExtraInformation: ULONG);
+  end;
+  RAWMOUSE = tagRAWMOUSE;
+
+  tagRAWHID = record
+    dwSizeHid: DWORD;
+    dwCount: DWORD;
+    bRawData: Byte;
+  end;
+
+  RAWHID = tagRAWHID;
+
+  tagRAWINPUT = record
+    header: RAWINPUTHEADER;
+    case Integer of
+      RIM_TYPEMOUSE: (mouse: RAWMOUSE);
+      RIM_TYPEKEYBOARD:(keyboard: RAWKEYBOARD);
+      RIM_TYPEHID: (hid: RAWHID);
+  end;
+
+  RAWINPUT = tagRAWINPUT;
+
 
 //function ProcessIsElevated(Process: Cardinal): Boolean;
 function GetProcessNameFromWnd(Wnd: HWND): string;
@@ -172,6 +246,10 @@ function GetThreadPreferredUILanguages(dwFlags: DWORD; var pulNumLanguages: DWOR
 function SHGetFolderPath(hwndOwner: HWND; nFolder: Integer; hToken: THANDLE;
   dwFlags: DWORD; pszPath: PChar): HResult; stdcall;
   external 'Shell32.dll' name 'SHGetFolderPathW';
+function RegisterRawInputDevices(pRawInputDevices: PRAWINPUTDEVICE;
+  uiNumDevices: UINT; cbSize: UINT): BOOL; stdcall; external 'user32.dll';
+function GetRawInputData(hRawInput: HRAWINPUT; uiCommand: UINT; pData: Pointer; var pcbSize: UINT; cbSizeHeader: UINT): UINT; stdcall;
+  external 'user32.dll';
 
 
 implementation
